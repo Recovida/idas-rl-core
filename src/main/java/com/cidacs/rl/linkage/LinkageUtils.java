32867e2891b07fb8bc6ba190d3d1c6516528b830
Vinícius <matosvb@gmail.com>
2021-03-02 17:37:27 -0300

    Utilização de ponto e vírgula para separar as colunas do arquivo de saída

--- src/main/java/com/cidacs/rl/linkage/LinkageUtils.java
+++ src/main/java/com/cidacs/rl/linkage/LinkageUtils.java
@@ -8,51 +8,61 @@ import com.cidacs.rl.record.ColumnRecordModel;
 import com.cidacs.rl.record.RecordPairModel;
 
 public class LinkageUtils {
-    public String fromRecordPairToCsv(RecordPairModel recordPair){
+    public String fromRecordPairToCsv(RecordPairModel recordPair) {
         String csvResult = "";
-        for(ColumnRecordModel column: recordPair.getRecordA().getColumnRecordModels()){
+        for (ColumnRecordModel column : recordPair.getRecordA()
+                .getColumnRecordModels()) {
             if (column.isGenerated())
                 continue;
-            csvResult=csvResult+column.getOriginalValue()+",";
+            csvResult = csvResult + quote(column.getOriginalValue()) + ";";
         }
-        for(ColumnRecordModel column: recordPair.getRecordB().getColumnRecordModels()){
+        for (ColumnRecordModel column : recordPair.getRecordB()
+                .getColumnRecordModels()) {
             if (column.isGenerated())
                 continue;
-            csvResult=csvResult+column.getOriginalValue()+",";
+            csvResult = csvResult + quote(column.getOriginalValue()) + ";";
         }
-        csvResult = csvResult + String.format(Locale.ENGLISH, "%7.10f", recordPair.getScore());
+        csvResult = csvResult + String.format(Locale.ENGLISH, "%.2f",
+                100 * recordPair.getScore());
         return csvResult;
     }
 
-    public String getCsvHeaderFromRecordPair(ConfigModel config, RecordPairModel recordPair) {
+    public String getCsvHeaderFromRecordPair(ConfigModel config,
+            RecordPairModel recordPair) {
         String headerResult = "";
-        for (ColumnRecordModel column: recordPair.getRecordA().getColumnRecordModels()){
+        for (ColumnRecordModel column : recordPair.getRecordA()
+                .getColumnRecordModels()) {
             if (column.isGenerated())
                 continue;
-            headerResult = headerResult + quote(column.getId() + "_" + config.getSuffixA()) + ",";
+            headerResult = headerResult
+                    + quote(column.getId() + "_" + config.getSuffixA()) + ";";
         }
-        for (ColumnRecordModel column: recordPair.getRecordB().getColumnRecordModels()){
+        for (ColumnRecordModel column : recordPair.getRecordB()
+                .getColumnRecordModels()) {
             if (column.isGenerated())
                 continue;
-            headerResult = headerResult + quote(column.getId() + "_" + config.getSuffixB()) + ",";
+            headerResult = headerResult
+                    + quote(column.getId() + "_" + config.getSuffixB()) + ";";
         }
         headerResult = headerResult + quote("score");
         return headerResult;
     }
 
-    public static String getCsvHeaderFromConfig(ConfigModel config){
+    public static String getCsvHeaderFromConfig(ConfigModel config) {
         String headerResult = "";
         // for each column a add to result
-        for (ColumnConfigModel col: config.getColumns()){
+        for (ColumnConfigModel col : config.getColumns()) {
             if (col.isGenerated())
                 continue;
-            headerResult = headerResult + quote(col.getIndexA() + "_" + config.getSuffixA()) + ",";
+            headerResult = headerResult
+                    + quote(col.getIndexA() + "_" + config.getSuffixA()) + ";";
         }
         // for each column b add to result
-        for (ColumnConfigModel col: config.getColumns()){
+        for (ColumnConfigModel col : config.getColumns()) {
             if (col.isGenerated())
                 continue;
-            headerResult = headerResult + quote(col.getIndexB() + "_" + config.getSuffixB()) + ",";
+            headerResult = headerResult
+                    + quote(col.getIndexB() + "_" + config.getSuffixB()) + ";";
         }
         headerResult = headerResult + quote("score");
         return headerResult;

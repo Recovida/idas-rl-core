ff7b0c919004a9cfc975c925dc59df53098804d4
Vinícius <matosvb@gmail.com>
2021-05-26 18:58:10 -0300

    Adaptação na exibição das mensagens que facilitará a integração com a interface gráfica

--- src/main/java/recovida/idas/rl/util/StatusReporter.java
+++ src/main/java/recovida/idas/rl/util/StatusReporter.java
@@ -26,34 +26,52 @@ public class StatusReporter {
         }
     }
 
-    public static class ConsoleLogger {
+    public interface StatusLogger {
 
-        public static void warn(String message) {
+        void warn(String message);
+
+        void info(String message);
+
+        void infoWithoutLineBreak(String message);
+
+        void error(String message);
+
+        void debug(String message);
+
+    }
+
+    public static class ConsoleLogger implements StatusLogger {
+
+        @Override
+        public void warn(String message) {
             log(System.err, "WARN", message);
         }
 
-        public static void info(String message) {
+        @Override
+        public void info(String message) {
             log(System.out, "INFO", message);
         }
 
-        public static void infoWithoutLineBreak(String message) {
+        @Override
+        public void infoWithoutLineBreak(String message) {
             log(System.out, "INFO", message, false);
         }
 
-        public static void error(String message) {
+        @Override
+        public void error(String message) {
             log(System.err, "ERROR", message);
         }
 
-        public static void debug(String message) {
+        @Override
+        public void debug(String message) {
             log(System.err, "DEBUG", message);
         }
 
-        public static void log(PrintStream stream, String type,
-                String message) {
+        public void log(PrintStream stream, String type, String message) {
             log(stream, type, message, true);
         }
 
-        public static void log(PrintStream stream, String type, String message,
+        public void log(PrintStream stream, String type, String message,
                 boolean lineBreak) {
             stream.format("[%8s] %5s: %s" + (lineBreak ? "%n" : ""),
                     LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS).format(
@@ -64,6 +82,7 @@ public class StatusReporter {
     }
 
     public static LoggingLevel currentLevel = LoggingLevel.INFO;
+    public static StatusLogger logger = new ConsoleLogger();
 
     protected static StatusReporter instance = null;
 
@@ -81,178 +100,175 @@ public class StatusReporter {
 
     public void warnIgnoringColumn(int number) {
         if (currentLevel.logs(LoggingLevel.WARN))
-            ConsoleLogger.warn(MessageFormat.format(
+            logger.warn(MessageFormat.format(
                     MessageProvider.getMessage("config.ignoringnum"), number));
     }
 
     public void errorConfigFileDoesNotExist(String configFileName) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("config.doesnotexist"),
                     configFileName));
     }
 
     public void infoUsingConfigFile(String configFileName) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("config.using"),
                     configFileName));
     }
 
     public void infoReadingA(String fileName) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("dataset.a.reading"), fileName));
     }
 
     public void infoReadingAndIndexingB(String fileName) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("dataset.b.reading"), fileName));
     }
 
     public void errorMissingColumnInDatasetA(String column) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("dataset.a.missingcolumn"),
                     column));
     }
 
     public void errorMissingColumnInDatasetB(String column) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("dataset.b.missingcolumn"),
                     column));
     }
 
     public void infoAvailableColumnsInDatasetA(String column) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("dataset.a.columnlist"),
                     column));
     }
 
     public void infoAvailableColumnsInDatasetB(String column) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("dataset.b.columnlist"),
                     column));
     }
 
     public void infoFinishedIndexingB(long n) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("dataset.b.finishedreading"),
                     n));
     }
 
     public void infoReusingIndex(long n) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("dataset.b.reusingindex"), n));
     }
 
     public void infoOldIndexLacksColumns() {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(
+            logger.info(
                     MessageProvider.getMessage("dataset.b.incompleteindex"));
     }
 
     public void infoOldIndexIsCorrupt() {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger
-            .info(MessageProvider.getMessage("dataset.b.corruptindex"));
+            logger.info(MessageProvider.getMessage("dataset.b.corruptindex"));
     }
 
     public void infoFinishedReadingA(long n) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("dataset.a.finishedreading"),
                     n));
     }
 
     public void infoMaxRowsA(long n) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat
+            logger.info(MessageFormat
                     .format(MessageProvider.getMessage("linkage.maxrows"), n));
     }
 
     public void infoPerformingLinkage(int nThreads) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("linkage.linking"), nThreads));
     }
 
     public void infoCompleted(String resultPath) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger.info(MessageFormat.format(
+            logger.info(MessageFormat.format(
                     MessageProvider.getMessage("linkage.done"), resultPath));
     }
 
     public void errorDatasetFileDoesNotExist(String fileName) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("dataset.doesnotexist"),
                     fileName));
     }
 
     public void errorDatasetFileFormatIsUnsupported(String fileName) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("dataset.unsupportedformat"),
                     fileName));
     }
 
     public void errorDatasetFileCannotBeRead(String fileName, String encoding) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("dataset.couldnotread"),
                     fileName, encoding));
     }
 
     public void errorOldIndexCannotBeDeleted(String dir) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(MessageProvider
+            logger.error(MessageFormat.format(MessageProvider
                     .getMessage("dataset.b.cannotdeleteoldindex"), dir));
     }
 
     public void warnInvalidValueForType(String value, String type) {
         if (currentLevel.logs(LoggingLevel.WARN))
-            ConsoleLogger.warn(MessageFormat.format(
+            logger.warn(MessageFormat.format(
                     MessageProvider.getMessage("dataset.invalidvalue"), value,
                     type));
     }
 
     public void infoLinkageProgress(float done) {
         if (currentLevel.logs(LoggingLevel.INFO))
-            ConsoleLogger
-            .infoWithoutLineBreak(MessageFormat.format(
-                    MessageProvider.getMessage(
-                            "linkage.progresspercentage") + '\r',
-                    done));
+            logger.infoWithoutLineBreak(
+                    MessageFormat.format(
+                            MessageProvider.getMessage(
+                                    "linkage.progresspercentage") + '\r',
+                            done));
     }
 
     public void warnCouldNotLinkRow() {
         if (currentLevel.logs(LoggingLevel.WARN))
-            ConsoleLogger
-            .warn(MessageProvider.getMessage("linkage.cannotlink"));
+            logger.warn(MessageProvider.getMessage("linkage.cannotlink"));
     }
 
     public void errorCannotSaveResult() {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger
-            .error(MessageProvider.getMessage("linkage.cannotsave"));
+            logger.error(MessageProvider.getMessage("linkage.cannotsave"));
     }
 
     public void errorMissingFieldInConfigFile(String field) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("config.missingfield"), field));
     }
 
     public void errorCannotIndex(String dir) {
         if (currentLevel.logs(LoggingLevel.ERROR))
-            ConsoleLogger.error(MessageFormat.format(
+            logger.error(MessageFormat.format(
                     MessageProvider.getMessage("dataset.b.cannotindex"), dir));
     }
 

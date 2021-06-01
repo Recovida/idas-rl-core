21b84b22e65e7c667a57bf62036bcf3da2252e81
Vinícius <matosvb@gmail.com>
2021-06-01 14:14:02 -0300

    Adicionado texto explicativo ao README

--- README.md
+++ README.md
@@ -1,7 +1,74 @@
-# Cidacs-RL-SIM
+# IDaS-RL (Core)
+
+Esta é uma ferramenta de record linkage desenvolvida por uma equipe do projeto
+Recovida a partir do código
+do [Cidacs-RL](https://github.com/gcgbarbosa/cidacs-rl-v1).
+
+Uma interface gráfica está disponível no
+[repositório IDaS-RL GUI](https://gitlab.com/recovida/idas-rl-gui).
 
 
-This repository contains a version of
-[Cidacs-RL](https://github.com/gcgbarbosa/cidacs-rl-v1) adapted to the
-needs of this project. For information about the program, please see
-the original repository.
+## Execução
+
+Inicialmente, [instale o Java](https://www.java.com/pt-BR/download/manual.jsp)
+na versão 8 ou qualquer versão mais recente.
+
+Faça o download da última versão do IDaS-RL (Core) na página
+[Releases](https://gitlab.com/recovida/idas-rl-gui/-/releases) deste
+repositório.
+
+Dentro do diretório (pasta) em que o arquivo `.jar` estiver salvo, crie
+um subdiretório chamado `assets` contendo um arquivo de configurações chamado
+`config.properties`, seguindo o formato descrito pela [documentação](doc/).
+
+Abra uma janela de um terminal / prompt de comando / Powershell, a depender
+do sistema operacional, e entre no diretório do arquivo `.jar`.
+Para executar o programa, digite
+```
+java -jar idas-rl-core-packaged.jar
+```
+(substituindo `idas-rl-core-packaged.jar` pelo nome exato do arquivo salvo,
+  que varia conforme a versão).
+O programa será executado lendo o arquivo de configurações
+`assets/config.properties`.
+
+Caso queira utilizar um arquivo de configurações com outro nome ou em outra
+localização, passe o nome desse arquivo como o único argumento do programa,
+de acordo com o formato a seguir:
+```
+java -jar idas-rl-core-packaged.jar arquivo-de-configurações.properties
+```
+(substituindo `idas-rl-core-packaged.jar`
+  e `arquivo-de-configurações.properties`
+  pelos nomes exatos dos arquivos).
+
+
+## Compilação
+
+Este repositório utiliza o
+[Maven](https://maven.apache.org/) para gerenciar o programa, o processo de
+compilação e suas dependências.
+
+Estando na raiz do repositório, execute ```mvn compile``` para compilar
+o programa, ```mvn exec:java``` para executar o programa e ```mvn package```
+para gerar o arquivo `.jar` no diretório `target`.
+
+## Tradução
+
+Para adicionar suporte a um novo idioma na exibição de mensagens para o
+usuário, acrescente ao arquivo
+[languages.txt](src/main/resources/lang/languages.txt)
+uma nova linha com a
+[*language tag*](https://docs.oracle.com/javase/tutorial/i18n/locale/matching.html)
+correspondente ao idioma, e crie um arquivo com extensão `.properties` no
+diretório `src/main/resources/lang/` com as mensagens traduzidas. Utilize
+um dos idiomas existentes como base e mantenha o
+[formato](https://docs.oracle.com/javase/8/docs/api/java/text/MessageFormat.html)
+das mensagens com argumentos. Recomenda-se utilizar uma ferramenta como o
+[ResourceBundle Editor](https://marketplace.eclipse.org/content/resourcebundle-editor)
+para facilitar o processo.
+
+
+## Licença
+
+Os conteúdos deste repositório estão publicados sob a licença [MIT](LICENSE).
--- src/main/java/recovida/idas/rl/core/lang/MessageProvider.java
+++ src/main/java/recovida/idas/rl/core/lang/MessageProvider.java
@@ -1,25 +1,77 @@
 package recovida.idas.rl.core.lang;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
+import org.apache.commons.io.IOUtils;
+
+/**
+ * A class that deals with i18n and provides translated messages.
+ */
 public class MessageProvider {
 
-    public final static List<String> SUPPORTED_LANGUAGES = Collections
-            .unmodifiableList(Arrays.asList(new String[] { "en", "es", "pt" }));
-    public static final String DEFAULT_LANGUAGE = SUPPORTED_LANGUAGES
-            .get(Math.max(0, SUPPORTED_LANGUAGES
-                    .indexOf(Locale.getDefault().getLanguage())));
+    /**
+     * List of supported language tags.
+     */
+    public static final List<String> SUPPORTED_LANGUAGES = readAvailableLanguages();
 
-    protected static Locale currentLocale = new Locale(DEFAULT_LANGUAGE);
+    /**
+     * The language tag of the system language, if present
+     * {@link #SUPPORTED_LANGUAGES}; or the some language tag in
+     * {@link #SUPPORTED_LANGUAGES} that matches the system language (even if
+     * the country does not match), if available; or the first language on the
+     * list, as a fallback.
+     */
+    public static final String DEFAULT_LANGUAGE = getBestLanguage();
+
+    protected static Locale currentLocale = Locale
+            .forLanguageTag(DEFAULT_LANGUAGE);
+
     protected static ResourceBundle bundle = createBundle(currentLocale);
 
     public static void setLocale(Locale locale) {
         bundle = createBundle(locale);
+        currentLocale = locale;
+    }
+
+    public static Locale getLocale() {
+        return currentLocale;
+    }
+
+    protected static List<String> readAvailableLanguages() {
+        try {
+            return Collections.unmodifiableList(IOUtils
+                    .readLines(
+                            MessageProvider.class.getClassLoader()
+                                    .getResourceAsStream("lang/languages.txt"),
+                            Charset.forName("UTF-8"))
+                    .stream().filter(l -> !l.isEmpty())
+                    .collect(Collectors.toList()));
+        } catch (IOException e) {
+            System.err.println(
+                    "Could not read language files. The installation is corrupt.");
+            System.err.flush();
+            System.exit(1);
+            return null; // to avoid warning
+        }
+    }
+
+    protected static String getBestLanguage() {
+        Locale defaultLocale = Locale.getDefault();
+        // try exact match
+        int idx = SUPPORTED_LANGUAGES.indexOf(defaultLocale.toLanguageTag());
+        if (idx < 0) // try language match, ignoring country
+            idx = SUPPORTED_LANGUAGES.stream().map(x -> x.split("-", 2)[0])
+                    .collect(Collectors.toList())
+                    .indexOf(defaultLocale.getLanguage());
+        // as a last resort, use the first language as a fallback
+        return SUPPORTED_LANGUAGES.get(Math.max(0, idx));
     }
 
     protected static ResourceBundle createBundle(Locale locale) {
